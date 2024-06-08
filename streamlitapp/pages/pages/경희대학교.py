f7f58f46bbe64c418bd641852cb53847b22e53e8
iamgone-22 <cgw0325@naver.com>
2024-06-08 11:03:43 +0900

    Add files via upload

---
+++ streamlitapp/pages/pages/경희대학교.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.subheader('경희대학교')
+st.markdown('최저 기준/탐구 과목 반영')
+st.text('교과/학생부 종합전형')
+st.text('2합 5(인문/자연)')
+st.text('3합 4(의예/한의예/약학/치예)')
+st.text('1단계: 학생부 100 2단계:1단계 성적 70% 면접 30%')
---
+++ streamlitapp/pages/pages/고려대학교.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.subheader('고려대학교')
+st.markdown('최저 기준/탐구 과목 반영')
+st.text('교과/학생부 종합전형')
+st.text('3합 7(학교추천형), 상위 1과목')
+st.text('4,합 8(학업우수형),2과목 평균')
+st.text('1단계: 서류 평가 100%(학업우수형)')
